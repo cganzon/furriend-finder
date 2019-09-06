9e67a51a9e2fe665069e75196228a0dbd6ea43db
cganzon <crganzon@gmail.com>
2019-09-05 19:00:21 -0700

    database creation

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Database connection
+mongoose
+  .connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/furriendfinderDB', { useNewUrlParser: true, useCreateIndex: true })
+  .then(() => console.log("MongoDB Connected!"))
+  .catch(err => console.error(err));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
